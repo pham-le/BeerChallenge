15f531a29b137f1716b820f0c3018027784c0b6f
Jannette Pham-Le <jannettephamle@gmail.com>
2017-04-16 01:31:24 -0700

    GET requests using URL for a user to get a info on a specific player or team

--- src/main/java/jpl/beerchallenge/web/GameController.java
+++ src/main/java/jpl/beerchallenge/web/GameController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jpl.beerchallenge.domain.Game;
@@ -137,4 +139,38 @@ public class GameController {
 
 		return new ModelAndView("scoreboard", "model", model);
 	}
+	
+	@RequestMapping(value = "/scoreboard/teamname={teamName}", method = RequestMethod.GET)
+	@ResponseBody
+	public String findTeamName(@PathVariable String teamName) {
+		String responseBody = "";
+		for (Game g : gameService.getGames()) {
+			if (g.getTeamName().equals(teamName)) {
+				responseBody = responseBody + g.toString() + "\n";
+			}
+		}
+		
+		if (responseBody.equals(""))
+			responseBody = "ERROR: Cannot find team(s) with name: " + teamName;
+		
+		return responseBody;
+	}
+	
+	@RequestMapping(value = "/scoreboard/playername={playerName}", method = RequestMethod.GET)
+	@ResponseBody
+	public String findPlayer(@PathVariable String playerName) {
+		String responseBody = "";
+		for (Player p : playerService.getPlayers()) {
+			if (p.getName().equals(playerName)) {
+				responseBody = responseBody + p.toString() + "\n";
+			}
+		}
+		
+		if (responseBody.equals(""))
+			responseBody = "ERROR: Cannot find player(s) with name: " + playerName;
+		
+		return responseBody;
+	}
 }
+
+
